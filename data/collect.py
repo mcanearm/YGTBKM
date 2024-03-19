0712b8eb9926e2cd86b6fbdbf039cfeece3c02ed
Matt McAnear <mcanearm@gmail.com>
2024-03-19 15:14:41 -0400

    update collection script with mapping of columns

--- data/collect.py
+++ data/collect.py
@@ -5,17 +5,10 @@ import requests
 
 # Y
 
-# body measures: weight, BMI at waist https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BMX.XPT
-# Bone mineral density: measure of bone health https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DXXFEM.XPT
-# Blood presure: systolic and diastolic https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BPXO.XPT
-
-# body measures: weight, BMI at waist https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BMX.XPT
-# Bone mineral density: measure of bone health https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DXXFEM.XPT
-# Blood presure: systolic and diastolic https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BPXO.XPT
-# physical activity: https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_PAQ.XPT (among adults)
-# alcohol use: https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_ALQ.XPT
-# Smoking: just do cigarettes/day: https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_SMQ.XPT
-# pre-existing conditions:
+# DONE Demographic: Demographic Variables and Sample Weights – Participant gender, age, race
+# DONE Dietary: Dietary Supplement Use 30-Day - Individual and Total Dietary Supplements – Caffeine (mg)
+# DONE Examination: Body Measures - Weight (kg)
+# DONE Questionnaire: Alcohol Use, Diabetes, Income,  Occupation, Physical Activity, Smoking - Cigarette Use, Kidney Conditions - Urology
 
 
 def read_sas_url(url):
@@ -23,16 +16,82 @@ def read_sas_url(url):
     return pd.read_sas(io.BytesIO(r.content), format="xport")
 
 
-{
-    "demographics": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DEMO.XPT",
-    "diet": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DBQ.XPT",
-    "body_measures": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BMX.XPT",
-    "bone_mineral_density": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DXXFEM.XPT",
-    "blood_presure": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BPXO.XPT",
-    "physical_activity": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_PAQ.XPT",
-    "alcohol_use": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_ALQ.XPT",
-    "smoking": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_SMQ.XPT",
-    "income": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_INQ.htm",
+column_mapping = {
+    "demographics": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DEMO.XPT",
+        "columns": [
+            "RIAGENDR",  # gender (sex)
+            "RIDRETH3",  # race including Asian
+            "RIDEXAGM",  # age in months
+        ],
+    },
+    "caff_intake1": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DR1IFF_J.XPT",
+        "columns": ["DR1ICAFF"],  # caffeine in mg
+    },
+    "caff_intake2": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DR2IFF_J.XPT",
+        "columns": ["DR2ICAFF"],  # caffeine in mg
+    },
+    "caff_supplement1": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DS2TOT_J.XPT",
+        "columns": ["DSQTCAFF"],  # caffeine in mg
+    },
+    "body_measures": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_BMX.XPT",
+        "columns": ["BMXBMI", "BMXWAIST"],  # BMI  # waist circumference
+    },
+    "alcohol": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_ALQ.XPT",
+        "columns": ["ALQ130"],  # avg # of drinks last year
+    },
+    "diabetes": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_DIQ.XPT",
+        "columns": [
+            "DIQ010",  # doctor told you you have diabetes,
+            "DIQ050",  # taking insulin
+            "DIQ280",  # last A1C level
+        ],
+    },
+    "income": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_INQ.XPT",
+        "columns": [
+            "INDFMMPI",  # multiple of poverty level,
+            "INDFMMPC",  # poverty level category
+        ],
+    },
+    "kidney": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_KIQ_U.XPT",
+        "columns": [
+            "KIQ026",  # ever had kidney stones
+            "KIQ029",  # passed kidney stone in 12/mo
+            "KIQ022",  # weak or failing kidneys
+        ],
+    },
+    "employment": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_OCQ.XPT",
+        "columns": ["OCQ180", "OCQ670"],  # hours worked in last week  # work schedule
+    },
+    "physical_act": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_PAQ.XPT",
+        "columns": [
+            "PAQ605",  # vigorous work activity
+            "PAQ620",  # moderate work activity
+            "PAQ635",  # walk or use a bicycle to get places at least 10 minutes
+            "PAD645",  # minutes walk/bicycle for transportation
+            "PAD660",  # minutes of vigorous physical activities
+            "PAD675",  # minutes moderate physical activities
+            "PAD680",  # minutes sedentary activity
+        ],
+    },
+    "smoking": {
+        "url": "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/P_SMQ.XPT",
+        "columns": [
+            "SMQ020",  # smoked at least 100 cigarettes in life
+            "SMD650",  # avg cigarettes/day smoked in the last 30 days
+        ],
+    },
 }
+
 
 # pre-existing conditions:
